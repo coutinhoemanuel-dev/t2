80678d128dc2ee79a3b075a9151b60cd8cde9aea
coutinhoemanuel-dev <coutinho.emanuel@escola.pr.gov.br>
2026-08-22 07:41:05 +0000

    Pending changes exported from your codespace

--- painel.php
+++ painel.php
@@ -4,18 +4,28 @@ include('protect.php');
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="painel_style.css">
     <title>Painel</title>
 </head>
 <body>
-    Bem vindo ao Painel, <?php echo $_SESSION['nome']; ?>.
-
-    <p>
-        <a href="logout.php">Sair</a>
-    </p>
+    <header>
+        <div class="div1">
+            <div class="div2">
+            <h1 class="h1_1">AGENDA TEC</h1>
+            <h2 class="h2_1">//RESERVA DE RECURSOS</h2>
+            </div>
+                <div class="div3">
+                <p class="p1">início</p>
+                <p class="p1">Meus Agendamentos</p>
+                <p class="p1">Recursos</P>
+                <p class="p1">relatorio</p>
+                </div>
+        </div>
+    </header>
 </body>
 </html>
